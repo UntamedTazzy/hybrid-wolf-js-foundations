e263a4cf567495a9531e798472b1bca81779da6e
Taz <wolftaztv@gmail.com>
2026-02-22 11:52:54 -0800

    Day 1 complete - variables foundation

---
+++ week1/practice.js
@@ -0,0 +1,13 @@
+// Day 1: Variables + Data Types
+
+const playerName = "Taz";
+let xp = 120;
+let level = 3;
+let isAlive = true;
+
+xp = xp + 50;
+
+console.log("Player:", playerName);
+console.log("XP:", xp);
+console.log("Level:", level);
+console.log("Alive?", isAlive);
